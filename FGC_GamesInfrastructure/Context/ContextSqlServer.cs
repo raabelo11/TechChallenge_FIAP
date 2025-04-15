bacd7437612e3220be1f42614cc479391f56fefa
raabelo11 <guilhermepainn@hotmail.com>
2025-04-14 21:03:16 -0300

    Adicionar arquivos de projeto.

---
+++ FGC_GamesInfrastructure/Context/ContextSqlServer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FGC_Games.Infrastructure.Context
+{
+    class ContextSqlServer
+    {
+    }
+}
